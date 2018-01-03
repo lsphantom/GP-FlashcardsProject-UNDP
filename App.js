249cf4fe890a4c56fff2c866e38849140fde8d58
Gary Pacheco <gpacheco@venom.comet.ucar.edu>
2018-01-02 19:24:27 -0700

    add stackview

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Text, View, StyleSheet, StatusBar, Platform } from 'react-native';
-import { TabNavigator } from 'react-navigation';
+import { Text, View, StyleSheet, StatusBar, Platform, TouchableOpacity } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 import { MaterialIcons } from '@expo/vector-icons';
 import { purple, orange, white } from './utils/colors';
 import { Constants } from 'expo';
+import DeckView from './components/DeckList';
 
 
 function UdaciCardsStatusBar ({ backgroundColor, ...props }) {
@@ -16,36 +17,48 @@ function UdaciCardsStatusBar ({ backgroundColor, ...props }) {
 
 
 
-function Decks() {
+function Decks({ navigation }) {
   return(
-    <View style={styles.container}>
-      <Text>DECKS SCREEN</Text>
+    <View>
+      <View style={styles.deckbox}>
+      <TouchableOpacity onPress={() => navigation.navigate('Deck')}>
+      <Text style={styles.decktitle}>
+        DECK NAME HERE
+      </Text>
+      </TouchableOpacity>
+      <Text style={styles.cardcount}>3 cards</Text>
+      </View>
+      <View style={styles.deckbox}>
+      <Text style={styles.decktitle}>DECK NAME</Text>
+      <Text style={styles.cardcount}>4 cards</Text>
+      </View>
     </View>
   )
 }
-  
+
 
 
 function newDeck() {
   return(
   <View style={styles.container}>
-    <Text>NEW DECK SCREEN</Text>
+    <Text style={styles.decktitle}>NEW DECK SCREEN</Text>
+    <Text style={styles.cardcount}></Text>
   </View>
  )
-} 
+}
 
 
 const Tabs = TabNavigator({
   Decks: {
-    screen: Decks,
+    screen: DeckView,
     navigationOptions: {
-      tabBarIcon: () => <MaterialIcons name="library-books" size={30} color='black' />
+      tabBarIcon: () => <MaterialIcons name="library-books" size={28} color='black' />
     }
   },
   NewDeck: {
     screen: newDeck,
     navigationOptions: {
-      tabBarIcon: () => <MaterialIcons name="library-add" size={30} color='black' />
+      tabBarIcon: () => <MaterialIcons name="library-add" size={28} color='black' />
     }
   } }, {
   navigationOptions: {
@@ -80,11 +93,27 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  deckbox: {
+    backgroundColor: '#6A6B7C',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 30,
+    paddingBottom: 30,
+    marginBottom: 2,
+  },
+  decktitle: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  cardcount: {
+    color: 'white',
+  },
+
 });
