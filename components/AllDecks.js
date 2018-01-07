8ef0bd0610e8a292cf27f1530b656406271e9bf6
Gary <gp@limestripes.us>
2018-01-06 23:21:46 -0700

    all decks display

---
+++ components/AllDecks.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {View, Text, Stylesheet, TouchableOpacity, FlatList} from 'react-native'
+import {StackNavigator} from 'react-navigation'
+import {styles} from '../styles'
+import {connect} from 'react-redux'
+import DeckView from './DeckView'
+import DeckList from './DeckList'
+
+
+
+const DeckStack = StackNavigator({
+  List: {
+    screen: DeckList,
+    navigationOptions: {
+      title: 'Decks',
+      headerTintColor: '#02b3e4'
+    },
+  },
+  Deck: {
+    screen: DeckView,
+    navigationOptions: {
+      headerTintColor: '#02b3e4'
+    },
+  }
+})
+
+
+
+class AllDecks extends React.Component {
+  render() {
+    return(
+    <View style={{flex:1}}>
+        <DeckStack />
+    </View>
+    )
+  }
+}
+
+
+function mapStateToProps(udaciCards){
+  return { udaciCards }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllDecks);
